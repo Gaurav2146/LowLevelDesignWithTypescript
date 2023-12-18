eb7164df3b4703ae97eb9837631bbcdaf0f6df22
Gaurav Kumar <GauravKumar@winjittechnologiespvtltd.local>
2023-12-18 17:31:15 +0530

    first commit

--- LOW_LEVEL_DESIGN/Chain_Of_Responsibility/Main.ts
+++ LOW_LEVEL_DESIGN/Chain_Of_Responsibility/Main.ts
@@ -1,11 +1,30 @@
 import { DebugLogging } from "./DebugLogging";
 import { ErrorLogging } from "./ErrorLogging";
 import { InfoLogging } from "./InfoLogging";
+const readline = require('readline');
 
+// Create an interface to read from the terminal
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 let logging = new InfoLogging(new DebugLogging(new ErrorLogging(null)));
 
-logging.log(1,"This is First log");
-logging.log(2,"This is second log");
-logging.log(3,"This is third log");
-logging.log(4,"This is fourth log");
+function askForInput() {
+// Prompt the user for input
+rl.question('Enter log type ', (logType: number) => {
+    rl.question('Enter log message ', (logMessage: string) => {
+            logging.log(logType, logMessage);
+            askForInput();
+    });
+});
+}
+
+askForInput();
+
+// rl.close();
+// // Event listener for when the interface is closed
+// rl.on('close', () => {
+//     console.log('Input interface closed.');
+// });
